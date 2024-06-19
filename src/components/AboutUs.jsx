0d34406b682506ade870da3233d2dff8dce063d2
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2024-06-19 19:11:13 +0500

    19-06-2024

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -4,7 +4,7 @@ const AboutUs = () => {
   return (
     <>
       <div className="About-container">
-        <h1>Our History</h1>
+        {/* <h1>Our History</h1>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
           repellat recusandae accusantium sapiente natus sit perspiciatis velit
@@ -20,7 +20,42 @@ const AboutUs = () => {
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
           repellat recusandae.
-        </p>
+        </p> */}
+        {/* ********************************************************** */}
+
+        <section className="cards-wrapper">
+          <div className="card-grid-space">
+            <div className="card" >
+              <div>
+                <h1>Our History</h1>
+                <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          repellat recusandae accusantium sapiente natus sit perspiciatis velit
+          repellendus unde quibusdam aut, iure expedita, itaque incidunt vitae
+          nemo? Possimus, vel ex
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="card-grid-space">
+            <div className="card" >
+              <div>
+                <h1>Our Mission</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                repellat recusandae  expedita, itaque incidunt vitaeex.</p>
+              </div>
+            </div>
+          </div>
+          <div className="card-grid-space">
+            <div className="card" >
+              <div>
+                <h1>Our Vision</h1>
+                <p>  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                repellat recusandae lorem50</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
