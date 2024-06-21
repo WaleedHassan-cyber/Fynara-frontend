2c1912d7905e68013596115c7b8e2cc78a3f0bed
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2024-06-21 19:57:05 +0500

    21-06-24

--- src/components/Curtains.jsx
+++ src/components/Curtains.jsx
@@ -13,7 +13,7 @@ const Curtains = () => {
         />
       <ProductCard
           badge="Hot"
-          src="https://images.unsplash.com/photo-1595777457583-95e059d581b8?q=80&w=1383&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="src/assets/Cutain1.jpeg"
           pc="Vallentine Frok"
           pa="Pure red dress"
           sprice="Rs4000"
@@ -21,7 +21,7 @@ const Curtains = () => {
         />
       <ProductCard
           badge="Hot"
-          src="https://images.unsplash.com/photo-1595777457583-95e059d581b8?q=80&w=1383&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="src/assets/Cutain1.jpeg"
           pc="Vallentine Frok"
           pa="Pure red dress"
           sprice="Rs4000"
@@ -29,7 +29,7 @@ const Curtains = () => {
         />
       <ProductCard
           badge="Hot"
-          src="https://images.unsplash.com/photo-1595777457583-95e059d581b8?q=80&w=1383&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="src/assets/Cutain1.jpeg"
           pc="Vallentine Frok"
           pa="Pure red dress"
           sprice="Rs4000"
@@ -37,20 +37,13 @@ const Curtains = () => {
         />
       <ProductCard
           badge="Hot"
-          src="https://images.unsplash.com/photo-1595777457583-95e059d581b8?q=80&w=1383&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="src/assets/Cutain1.jpeg"
           pc="Vallentine Frok"
           pa="Pure red dress"
           sprice="Rs4000"
           oprice="Rs3400"
         />
-      <ProductCard
-          badge="Hot"
-          src="https://images.unsplash.com/photo-1595777457583-95e059d581b8?q=80&w=1383&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          pc="Vallentine Frok"
-          pa="Pure red dress"
-          sprice="Rs4000"
-          oprice="Rs3400"
-        />
+      
         
     </>
   )
