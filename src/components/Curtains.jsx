e1b0ddc9845b125517274f0d7306c4674598c82d
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2024-06-22 17:21:48 +0500

    22-06-24

--- src/components/Curtains.jsx
+++ src/components/Curtains.jsx
@@ -6,8 +6,8 @@ const Curtains = () => {
       <ProductCard
           badge="Hot"
           src="src/assets/Cutain1.jpeg"
-          pc="Vallentine Frok"
-          pa="Pure red dress"
+          pc="Valvet Curtain"
+          pa="Blue with Black Color"
           sprice="Rs4000"
           oprice="Rs3400"
         />
