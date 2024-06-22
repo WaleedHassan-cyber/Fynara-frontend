cfa17681868fb9944765f8711d2f8ecc249e65da
WaleedHassan-cyber <waleedhassan3722@gmail.com>
2024-06-22 17:35:57 +0500

    22-06-24

--- src/components/Curtains.jsx
+++ src/components/Curtains.jsx
@@ -1,52 +1,50 @@
-import React from 'react'
-import ProductCard from './ProductCard'
+import React from "react";
+import ProductCard from "./ProductCard";
 const Curtains = () => {
   return (
     <>
       <ProductCard
-          badge="Hot"
-          src="src/assets/Cutain1.jpeg"
-          pc="Valvet Curtain"
-          pa="Blue Velvet"
-          sprice="Rs4000"
-          oprice="Rs3400"
-        />
+        badge="Hot"
+        src="src/assets/Cutain1.jpeg"
+        pc="Valvet Curtain"
+        pa="Blue Velvet"
+        sprice="Rs4000"
+        oprice="Rs3400"
+      />
       <ProductCard
-          badge="Hot"
-          src="src/assets/Cutain1.jpeg"
-          pc="Vallentine Frok"
-          pa="Pure red dress"
-          sprice="Rs4000"
-          oprice="Rs3400"
-        />
+        badge="Hot"
+        src="https://hbfabrics.b-cdn.net/wp-content/uploads/2022/11/Luxury-Velvet-Curtains-9.jpg"
+        pc="Vallentine Frok"
+        pa="Pure red dress"
+        sprice="Rs4000"
+        oprice="Rs3400"
+      />
       <ProductCard
-          badge="Hot"
-          src="src/assets/Cutain1.jpeg"
-          pc="Vallentine Frok"
-          pa="Pure red dress"
-          sprice="Rs4000"
-          oprice="Rs3400"
-        />
+        badge="Hot"
+        src="https://mehshaninteriors.com/wp-content/uploads/2022/05/WhatsApp-Image-2022-05-11-at-7.36.00-PM.jpeg"
+        pc="Versace Velvet Curtain"
+        pa="Black on Gray"
+        sprice="Rs4000"
+        oprice="Rs3400"
+      />
       <ProductCard
-          badge="Hot"
-          src="src/assets/Cutain1.jpeg"
-          pc="Vallentine Frok"
-          pa="Pure red dress"
-          sprice="Rs4000"
-          oprice="Rs3400"
-        />
+        badge="Hot"
+        src="src/assets/Cutain1.jpeg"
+        pc="Vallentine Frok"
+        pa="Pure red dress"
+        sprice="Rs4000"
+        oprice="Rs3400"
+      />
       <ProductCard
-          badge="Hot"
-          src="src/assets/Cutain1.jpeg"
-          pc="Vallentine Frok"
-          pa="Pure red dress"
-          sprice="Rs4000"
-          oprice="Rs3400"
-        />
-      
-        
+        badge="Hot"
+        src="src/assets/Cutain1.jpeg"
+        pc="Vallentine Frok"
+        pa="Pure red dress"
+        sprice="Rs4000"
+        oprice="Rs3400"
+      />
     </>
-  )
-}
+  );
+};
 
-export default Curtains
+export default Curtains;
